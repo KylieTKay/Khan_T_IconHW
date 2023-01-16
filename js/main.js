54999d035e10655bc321d3c9e9c4606d964a7eb3
KylieTKay <113371037+KylieTKay@users.noreply.github.com>
2023-01-16 18:19:35 -0500

    images setup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log('JavaScript is running!');
+
+// step 1 - select element to enteract with 
+let bitmapGraphic = document.querySelector("#bitmapGraphic");
+
+// step 3- what happens when events are triggered
+function logID() {
+    console.log(this.id);
+}
+
+// step 2 - how do users interact with element
+bitmapGraphic.addEventListener('click', logID);
+
